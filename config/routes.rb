51dae28a54d4eca502ef064b3f12ced54e0b00a5
bhagathreddy123 <katta_reddy04@yahoo.com>
2021-04-24 04:42:11 -0700

    Bar chart

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   get 'residential/index'
   get 'residential/data', :defaults => { :format => 'json'}
   root :to => 'residential#index'
+
+  get 'residential/bar_chart'
+  get 'residential/bar_data', :defaults => { :format => 'json' }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
